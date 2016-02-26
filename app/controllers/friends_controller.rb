3a67fb3f1e462075b27bab608c7c4aa903f5181e
Emma Koszinowski <emkosz@gmail.com>
2016-02-26 10:59:41 -0800

    Add new contacts

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,24 @@ class FriendsController < ApplicationController
   def index
     @friends = Friend.all
   end
+
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+    if @friend.save
+      flash[:success] = "Added new contact"
+      redirect_to friends_path
+    else
+      flash[:error] = "There was an error adding the new contact."
+      render action: :new
+    end
+  end
+
+  private
+  def friend_params
+    params[:friend].permit(:name, :note)
+  end
 end
