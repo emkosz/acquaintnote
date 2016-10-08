44dc2ca3e7b1fd03824680b40b241121194f4a70
Emma Koszinowski <emkosz@gmail.com>
2016-10-08 14:41:27 -0700

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
   root 'static_pages#home'
   get 'about'   =>    'static_pages#about'
   get 'contact' =>    'static_pages#contact'
