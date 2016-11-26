88ed1260f4df5c39db4df66a4c5281c775776f47
Emma Koszinowski <emkosz@gmail.com>
2016-11-26 15:45:33 -0800

    fix initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,7 +2,7 @@ require 'twitter'
 
 $twitter_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = Rails.application.secrets.twitter_app_id
-  config.consumer_secret     = Rails.application.twitter_app_secret
-  config.access_token        = Rails.application.twitter_access_token
-  config.access_token_secret = Rails.application.twitter_access_token_secret
+  config.consumer_secret     = Rails.application.secrets.twitter_app_secret
+  config.access_token        = Rails.application.secrets.twitter_access_token
+  config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
 end
